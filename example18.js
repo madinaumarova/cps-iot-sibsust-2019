2b7a896886678a16b82fb5ea7ff739d28ca82ccc
magsver9 <ms.constanta360@gmail.com>
2019-12-10 17:54:51 +0700

    Description of commit, e.g. examples from 10/12/19

--- example18.js
+++ example18.js
@@ -31,7 +31,7 @@ var board = new firmata.Board("/dev/ttyACM0", function(){
 });
 
 function handler(req, res) {
-    fs.readFile(__dirname + "/example15.html",
+    fs.readFile(__dirname + "/example18.html",
     function (err, data) {
         if (err) {
             res.writeHead(500, {"Content-Type": "text/plain"});
@@ -45,11 +45,12 @@ function handler(req, res) {
 var desiredValue = 0; // desired value var
 var actualValue = 0; // variable for actual value (output value)
 
-var Kp = 0.55; // proportional factor of PID controller
-var Ki = 0.008; // integral factor of PID controller
-var Kd = 0.15; // differential factor of PID controller
+var Kp1 = 0.55; // proportional factor of PID controller
+var Ki1 = 0.008; // integral factor of PID controller
+var Kd1 = 0.15; // differential factor of PID controller
 
-var factor = 3; // proportional factor that determines speed of resonse
+var factor = 0.3; // proportional factor that determines speed of resonse
+
 var pwm = 0; // set pwm as global variable
 var pwmLimit = 254; // to limit value of the pwm that is sent to the motor
 
@@ -69,18 +70,20 @@ board.on("ready", function() {
         actualValue = value; // continuous read of pin A1
     });
     
-    startControlAlgorithm(); // to start control alg.
-    
     wss.on('connection', function (ws, req) { // start of wss code
         messageJSON = {"type": "message", "content": "Srv connected, board OK"};
         ws.send(JSON.stringify(messageJSON));
+        messageJSON = {"type": "staticMsgToClient", "content": "Srv connected, board OK"};
+        ws.send(JSON.stringify(messageJSON));
+        
+        
         setInterval(sendValues, 40); // on 40ms we send message to client
         
         ws.on("message", function (msgString) { // message comes as string -> msgString
             var msg = JSON.parse(msgString); // string from ws which comes as a string is put to JSON
             switch(msg.type) {
                 case "startControlAlgorithm":
-                    startControlAlgorithm();
+                    startControlAlgorithm(msg); // msg has all values, alg. type and parameters
                 break;
                 case "stopControlAlgorithm":
                     stopControlAlgorithm();                
@@ -91,30 +94,43 @@ board.on("ready", function() {
 
 }); // end of board.on(ready)
 
-function controlAlgorithm () {
-    err = desiredValue - actualValue; // error as difference between desired and actual val.
-    errSum += err; // sum of errors | like integral
-    dErr = err - lastErr; // difference of error
-    pwm = Kp*err+Ki*errSum+Kd*dErr; // PID expression
-    lastErr = err; // save the value of error for next cycle to estimate the derivative
-    if(pwm > pwmLimit) {pwm = pwmLimit}; // to limit the value for pwm / positive
-    if(pwm < -pwmLimit) {pwm = -pwmLimit}; // to limit the value for pwm / negative
-    if (pwm > 0) {board.digitalWrite(2,1); board.digitalWrite(4,0);}; // determine direction if > 0
-    if (pwm < 0) {board.digitalWrite(2,0); board.digitalWrite(4,1);}; // determine direction if < 0
-    board.analogWrite(3, Math.abs(pwm));
-};
+function controlAlgorithm (msg) { // the parameter in the argument holds ctrlAlgNo and param. values
+    if(msg.ctrlAlgNo == 1) {
+        pwm = msg.pCoeff*(desiredValue-actualValue);
+        if(pwm > pwmLimit) {pwm = pwmLimit}; // to limit the value for pwm / positive
+        if(pwm < -pwmLimit) {pwm = -pwmLimit}; // to limit the value for pwm / negative
+        if (pwm > 0) {board.digitalWrite(2,1); board.digitalWrite(4,0);}; // določimo smer če je > 0
+        if (pwm < 0) {board.digitalWrite(2,0); board.digitalWrite(4,1);}; // določimo smer če je < 0
+        board.analogWrite(3, Math.abs(pwm));
+        console.log(Math.round(pwm));
+    }
+    if(msg.ctrlAlgNo == 2) {
+        err = desiredValue - actualValue; // error as difference between desired and actual val.
+        errSum += err; // sum of errors | like integral
+        dErr = err - lastErr; // difference of error
+        pwm = msg.Kp1*err+msg.Ki1*errSum+msg.Kd1*dErr; // PID expression
+        lastErr = err; // save the value of error for next cycle to estimate the derivative
+            if(pwm > pwmLimit) {pwm = pwmLimit}; // to limit the value for pwm / positive
+            if(pwm < -pwmLimit) {pwm = -pwmLimit}; // to limit the value for pwm / negative
+            if (pwm > 0) {board.digitalWrite(2,1); board.digitalWrite(4,0);}; // determine direction if > 0
+            if (pwm < 0) {board.digitalWrite(2,0); board.digitalWrite(4,1);}; // determine direction if < 0
+        board.analogWrite(3, Math.abs(pwm));
+        };
+}
 
-function startControlAlgorithm () {
+function startControlAlgorithm (msg) {
     if (controlAlgorithmStartedFlag == 0) {
         // reset parameters
         pwm = 0; // Reset Pulse Width Modulation value
         err = 0; // Reset error
         errSum = 0; // Reset sum of errors as integral
         dErr = 0; // Reset difference of error
-        lastErr = 0; // Reset value whih keeps the value of previous error to estimate derivative
+        lastErr = 0; // Reset value which keeps the value of previous error to estimate derivative
         controlAlgorithmStartedFlag = 1;
-        intervalCtrl = setInterval(function(){controlAlgorithm();}, 30); // call the alg. on 30ms
-        console.log("Control algorithm has been started.");        
+        intervalCtrl = setInterval(function(){controlAlgorithm(msg);}, 30); // call the alg. on 30ms
+        console.log("Control algorithm has been started.");
+        messageJSON = {"type": "staticMsgToClient", "content": " No. " + msg.ctrlAlgNo + " started | " + json2txt(msg)};
+        wss.broadcast(JSON.stringify(messageJSON));
     }
 };
 
@@ -122,9 +138,32 @@ function stopControlAlgorithm () {
     clearInterval(intervalCtrl); // clear the interval of control algorihtm
     board.analogWrite(3, 0);
     controlAlgorithmStartedFlag = 0;
+    pwm = 0;
     console.log("Control algorithm has been stopped.");
+    messageJSON = {"type": "staticMsgToClient", "content": "Stopped."};
+    wss.broadcast(JSON.stringify(messageJSON));
 };
 
 function sendValues () {
-    wss.broadcast(JSON.stringify({"type": "clientReadValues", "desiredValue": desiredValue, "actualValue": actualValue, "error": (desiredValue - actualValue), "pwm": (pwm).toFixed(0)}));
+    wss.broadcast(JSON.stringify({"type": "clientReadValues", "desiredValue": desiredValue, "actualValue": actualValue, "error": (desiredValue - actualValue), "pwm": (pwm).toFixed(0), "err": err,"errSum": errSum, "dErr": dErr}));
 };
+
+function json2txt(obj) // function to print out the json names and values
+{
+  var txt = '';
+  var recurse = function(_obj) {
+    if ('object' != typeof(_obj)) {
+      txt += ' = ' + _obj + '\n';
+    }
+    else {
+      for (var key in _obj) {
+        if (_obj.hasOwnProperty(key)) {
+          txt += '.' + key;
+          recurse(_obj[key]);
+        } 
+      }
+    }
+  };
+  recurse(obj);
+  return txt;
+}
